dd1a4dbd8596a462ef6e7d0a357ffae76e052ec2
Tom910 <tom910ru@gmail.com>
2016-08-28 14:35:05 +0300

    Added AsyncComponent

---
+++ src/AsyncComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+const errorLoading = (err) => console.error('AsyncComponent: Loading failed', err);
+const isFunction = (func) => Object.prototype.toString.call(func) === '[object Function]';
+
+export default (loader, { placeholder } = {}) => {
+  class AsyncComponent extends Component {
+    constructor() {
+      super();
+
+      this.state = {
+        component: null
+      };
+      this.mounting = true;
+    }
+
+    componentWillMount() {
+      if (!isFunction(loader)) {
+        return console.error('AsyncComponent: Loader is not function');
+      }
+
+      loader()
+        .then((module) => this.mounting && this.setState({ component: module.default }))
+        .catch(errorLoading);
+    }
+
+    componentWillUnmount() {
+      this.mounting = true;
+    }
+
+    render() {
+      if (this.state.component) {
+          return <this.state.component {...this.props} />
+      }
+
+      return (
+        placeholder || null
+      );
+    }
+  }
+
+  return AsyncComponent;
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import AsyncComponent from './AsyncComponent';
+
+export {
+  AsyncComponent
+}
